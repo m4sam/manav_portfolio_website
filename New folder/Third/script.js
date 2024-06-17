f2fca244b16431875436a553b3792233af69f3ed
Manav <128348285+m4sam@users.noreply.github.com>
2024-06-17 14:24:08 +0000

    some changes

--- New folder/Third/script.js
+++
@@ -1,9 +0,0 @@
-// Selector
-
-let localTime=document.getElementById("time");
-
-setInterval(()=>{
-let time=new Date();
-let newTime=time.toLocaleTimeString();
-localTime.innerHTML=newTime
-},1000)
